dd8569f27644b9b76991f3a33bf6f674a38a38bc
Michael Kreil <github@michael-kreil.de>
2024-02-14 12:49:52 +0100

    more fixes

--- README.md
+++ README.md
@@ -8,10 +8,9 @@ hcloud context create versatile
 # setup server
 ```bash
 NAME="download.versatiles.org1"
-SECRET=$(cat secret.txt)
 hcloud server create --location nbg1 --image debian-12 --type cax11 --name $NAME --network download.versatiles.org --volume download.versatiles.org --ssh-key 9919841
 sleep 30
-echo "SECRET=\"$SECRET\"" | cat - scripts/setup_server.sh | hcloud server ssh $NAME
+echo "SECRET=\"$(cat secret.txt)\"" | cat - scripts/setup_server.sh | hcloud server ssh $NAME
 hcloud load-balancer add-target download.versatiles.org --server $NAME --use-private-ip
 ```
 
--- scripts/setup_server.sh
+++ scripts/setup_server.sh
@@ -15,35 +15,34 @@ ufw allow OpenSSH
 ufw allow 8080/tcp
 ufw allow 9000/tcp
 ufw --force enable
-
-echo -e "${RED}ADD USER${NC}"
-mkdir /var/www/download.versatiles.org
-chown www-data /var/www/download.versatiles.org
-su - www-data -s /bin/bash
-cd ~
-git clone https://github.com/versatiles-org/download.versatiles.org.git
 git config --global --add safe.directory '*'
-cd download.versatiles.org
-npm install
-npm run start /var/www/docs
-exit
 
 echo -e "${RED}MOUNT VOLUME${NC}"
 echo "/dev/disk/by-id/scsi-0${VOLUME} /mnt/${VOLUME}/ ext4 ro,nosuid,dev,noexec,auto,nouser,async 0 0" >> /etc/fstab
 systemctl daemon-reload
 mkdir "/mnt/${VOLUME}/"
 mount "/mnt/${VOLUME}/"
+ln -s /mnt/HC_Volume_29360110/download/ /var/www/docs
+
+echo -e "${RED}ADD USER${NC}"
+mkdir /var/www/download.versatiles.org
+chown www-data /var/www/
+su - www-data -s /bin/bash
+cd /var/www/
+git clone https://github.com/versatiles-org/download.versatiles.org.git
+cd download.versatiles.org
+npm install
+npm run start /var/www/docs
+exit
 
 echo -e "${RED}CONFIG NGINX${NC}"
 mkdir /etc/nginx/sites
-mkdir /var/www/docs
 mkdir /var/www/logs
 rm -r /etc/nginx/sites-available
 rm -r /etc/nginx/sites-enabled
 rm /etc/nginx/nginx.conf
 ln -s /var/www/download.versatiles.org/config/nginx/nginx.conf /etc/nginx/nginx.conf
 ln -s /var/www/download.versatiles.org/config/nginx/download.versatiles.org.conf /etc/nginx/sites/download.versatiles.org.conf
-ln -s /mnt/HC_Volume_29360110/download /var/www/docs
 nginx -s reload
 
 echo -e "${RED}CONFIG WEBHOOK${NC}"

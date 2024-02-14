71cd931d1b8f62affc752c6b06a4337161c1c6e4
Michael Kreil <github@michael-kreil.de>
2024-02-14 13:59:29 +0100

    update server name

--- create_server.sh
+++ create_server.sh
@@ -2,7 +2,7 @@
 cd "$(dirname "$0")"
 set -e
 
-NAME="download.versatiles.org1"
+NAME="download.versatiles.org"
 hcloud server create --location nbg1 --image debian-12 --type cax11 --name $NAME --network download.versatiles.org --volume download.versatiles.org --ssh-key 9919841
 sleep 30
 echo "SECRET=\"$(cat secret.txt)\"" | cat - scripts/setup_server.sh | hcloud server ssh $NAME
--- delete_server.sh
+++ delete_server.sh
@@ -2,7 +2,7 @@
 cd "$(dirname "$0")"
 set -e
 
-NAME="download.versatiles.org1"
+NAME="download.versatiles.org"
 sed -i '' -e '/128\.140\.47\.180/d' ~/.ssh/known_hosts
 hcloud volume detach download.versatiles.org
 hcloud server delete $NAME

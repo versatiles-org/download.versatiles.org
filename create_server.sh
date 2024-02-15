d123fae94914c5e88cd8d0dc7868fe1103d2334e
Michael Kreil <github@michael-kreil.de>
2024-02-15 14:15:13 +0100

    set secret

--- create_server.sh
+++ create_server.sh
@@ -5,5 +5,5 @@ set -e
 NAME="download.versatiles.org"
 hcloud server create --location nbg1 --image debian-12 --type cax11 --name $NAME --network download.versatiles.org --volume download.versatiles.org --ssh-key 9919841
 sleep 30
-echo "SECRET=\"$(cat secret.txt)\"" | cat - scripts/setup_server.sh | hcloud server ssh $NAME
+cat secret.txt scripts/setup_server.sh | hcloud server ssh $NAME
 hcloud load-balancer add-target download.versatiles.org --server $NAME --use-private-ip

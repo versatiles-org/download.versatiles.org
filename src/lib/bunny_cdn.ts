97fb662ba1af7567943fff926222466bb0fd781b
Michael Kreil <github@michael-kreil.de>
2024-04-04 09:01:37 +0200

    fix: updateEdgeRules

--- src/lib/bunny_cdn.ts
+++ src/lib/bunny_cdn.ts
@@ -1,9 +1,23 @@
 import { Redirect } from './files.js';
 
 export async function updateEdgeRules(redirects: Redirect[]) {
+
+	const request = await fetch(
+		`https://api.bunny.net/pullzone/${process.env.BUNNY_PULLZONE_ID}`, {
+		method: 'GET',
+		headers: {
+			'AccessKey': String(process.env.BUNNY_API_KEY),
+			'Accept': 'application/json',
+		}
+	})
+	const response = await request.json();
+	const desc2guid = new Map<string, string>(response.EdgeRules.map((e: any) => [e.Description, e.Guid]));
+
+
 	for (const redirect of redirects) {
-		const body = JSON.stringify({
-			//Guid: redirect.link,
+		const desc = 'redirect to latest: ' + redirect.link;
+		const message = {
+			Guid: undefined as (undefined | string),
 			ActionType: 2,
 			ActionParameter1: 'https://download.versatiles.org/' + redirect.file.filename,
 			TriggerMatchingType: 0,
@@ -12,10 +26,11 @@ export async function updateEdgeRules(redirects: Redirect[]) {
 				PatternMatches: ['https://download.versatiles.org/' + redirect.link],
 				PatternMatchingType: 0,
 			}],
-			Description: 'redirect ' + redirect.link,
+			Description: desc,
 			Enabled: true
-		});
-		console.log(redirect, body);
+		};
+		if (desc2guid.has(desc)) message.Guid = desc2guid.get(desc);
+
 		const request = await fetch(
 			`https://api.bunny.net/pullzone/${process.env.BUNNY_PULLZONE_ID}/edgerules/addOrUpdate`, {
 			method: 'POST',
@@ -24,7 +39,7 @@ export async function updateEdgeRules(redirects: Redirect[]) {
 				'Accept': 'application/json',
 				'Content-Type': 'application/json',
 			},
-			body
+			body: JSON.stringify(message)
 		})
 		const response = await request.json();
 		console.log(response);

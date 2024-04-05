8a538ed101332b89a1044b403bb92f9862056ddb
Michael Kreil <github@michael-kreil.de>
2024-04-05 16:10:21 +0200

    remove unused script

--- scripts/make_urllist.sh
+++
@@ -1,7 +0,0 @@
-
-FILE=planet-latest.versatiles
-DIRE=/var/www/download.versatiles.org/docs
-SIZE=$(stat -Lc %s $DIRE/$FILE)
-HASH=$(pv $DIRE/$FILE | openssl dgst -md5 -binary | openssl enc -base64)
-
-echo -e "TsvHttpData-1.0\nhttps://download.versatiles.org/$FILE\t$SIZE\t$HASH\n" > $DIRE/urllist.tsv

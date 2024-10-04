8d57ac7e0e5b0caed70c4ecc295a3e6df9faff2d
Michael Kreil <github@michael-kreil.de>
2024-10-05 01:37:22 +0200

    feat: add server

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,9 @@ import { run } from './lib/run.js';
 
 const app = express();
 
-app.get('/update', (req, res) => {
-  run();
-  res.status(200).end();
+app.get('/update', (_req, res) => {
+	run();
+	res.status(200).end();
 })
+
+app.listen(80);

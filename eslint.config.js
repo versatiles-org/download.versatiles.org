0d8a8c4c2d23bc18d418f32a6a984ea75ff64592
Michael Kreil <github@michael-kreil.de>
2024-10-04 21:32:08 +0200

    fix: eslint

---
+++ eslint.config.js
@@ -0,0 +1,54 @@
+import js from '@eslint/js';
+import ts from 'typescript-eslint';
+import parser from '@typescript-eslint/parser';
+import eslint_plugin from '@typescript-eslint/eslint-plugin';
+
+export default [
+	{
+		ignores: [
+			'bin/**/*',
+			'coverage/**/*',
+			'dist/**/*',
+			'volumes/**/*',
+		]
+	},
+	js.configs.recommended,
+	...ts.configs.recommended,
+	{
+		files: [
+			'src/**/*.ts',
+		],
+		languageOptions: {
+			ecmaVersion: 'latest',
+			sourceType: 'module',
+			globals: {
+				browser: false,
+				es6: true,
+				node: true
+			},
+			parser,
+			parserOptions: {
+				sourceType: 'module',
+				project: './tsconfig.json',
+				tsconfigRootDir: import.meta.dirname,
+			},
+		},
+		plugins: {
+			'@typescript-eslint': eslint_plugin,
+		},
+		linterOptions: {
+			reportUnusedDisableDirectives: true,
+		},
+		rules: {
+			'no-unused-vars': 'off',
+			'@typescript-eslint/no-unused-vars': [
+				'error',
+				{
+					argsIgnorePattern: '^_',
+					varsIgnorePattern: '^_',
+					caughtErrorsIgnorePattern: '^_'
+				}
+			]
+		}
+	}
+]

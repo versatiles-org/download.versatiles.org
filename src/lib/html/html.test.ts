ff85f9fb0a6239ead4eff904b5780a28331a3b53
Michael Kreil <github@michael-kreil.de>
2024-10-11 15:52:55 +0200

    test: html

---
+++ src/lib/html/html.test.ts
@@ -0,0 +1,24 @@
+import { buildHTML } from './html.js';
+import { FileGroup } from '../file/file_group.js';
+
+describe('buildHTML', () => {
+
+	test('should build HTML using the Handlebars template', () => {
+		const fileGroups = [group(1), group(2)];
+		const result = buildHTML(fileGroups);
+		expect(result).toContain('<html>');
+		expect(result).toContain('a group with index 1');
+		expect(result).toContain('a group with index 2');
+	});
+
+	function group(index: number): FileGroup {
+		return {
+			slug: 'group_' + index,
+			title: 'Group ' + index,
+			desc: 'a group with index ' + index,
+			order: index * 10,
+			local: false,
+			olderFiles: []
+		}
+	}
+});

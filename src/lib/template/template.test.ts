9f99a435ee11cbefc50115bfd9076ca8b9d8a499
Michael Kreil <github@michael-kreil.de>
2025-11-23 17:09:25 +0100

    refactor: update template tests to use fileGroups function and include RSS generation

--- src/lib/template/template.test.ts
+++ src/lib/template/template.test.ts
@@ -1,25 +1,31 @@
 import { describe, expect, it } from 'vitest';
-import { renderTemplate } from './template.js';
+import { generateRSSFeeds, renderTemplate } from './template.js';
 import { FileGroup } from '../file/file_group.js';
+import { tmpdir } from 'os';
 
 describe('renderTemplate', () => {
-
-	it('should build HTML using the Handlebars template', () => {
-		const fileGroups = [group(1), group(2)];
-		const result = renderTemplate(fileGroups, "index.html");
-		expect(result).toContain('<html>');
-		expect(result).toContain('a group with index 1');
-		expect(result).toContain('a group with index 2');
-	});
-
-	function group(index: number): FileGroup {
-		return new FileGroup({
-			slug: 'group_' + index,
-			title: 'Group ' + index,
-			desc: 'a group with index ' + index,
-			order: index * 10,
+	function fileGroups(): FileGroup[] {
+		return [1, 3].map(i => new FileGroup({
+			slug: 'group_' + i,
+			title: 'Group ' + i,
+			desc: 'a group with index ' + i,
+			order: i * 10,
 			local: false,
 			olderFiles: []
-		})
+		}));
 	}
+
+	it('should render Template', () => {
+		const result = renderTemplate(fileGroups(), "index.html");
+		expect(result).toContain('<html>');
+		expect(result).toContain('a group with index 1');
+		expect(result).toContain('a group with index 3');
+	});
+
+	it('should generate RSS', () => {
+		const result = generateRSSFeeds(fileGroups(), tmpdir());
+		expect(result.length).toBe(2);
+		expect(result[0].filename).toBe("feed-group_1.xml");
+		expect(result[1].filename).toBe("feed-group_3.xml");
+	});
 });
